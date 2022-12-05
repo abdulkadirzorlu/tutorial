83f56d37444693052bd569e2d2ea9edca1896819
Kadir Zorlu <75729647+abdulkadirzorlu@users.noreply.github.com>
2022-12-05 10:26:08 +0300

    Nested Components and Tools

--- src/index.js
+++ src/index.js
@@ -5,18 +5,14 @@ import ReactDOM from "react-dom"
 function Greeting() {
     return (
         <div>
-            <h3>hello people</h3>
-            <ul>
-                <li>
-                    <a href='#'>hello world</a>
-                </li>
-                <img src='' alt='' />
-                <input type='text' name='' id='' />
-                I
-            </ul>
+            <Person />
+            <Message />
         </div>
     );
 }
-
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+    return <p>this is my message</p>;
+};
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));

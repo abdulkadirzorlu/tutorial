4ab4043e67f98c4c19a29ebb592908bcfaffa2fd
Kadir Zorlu <75729647+abdulkadirzorlu@users.noreply.github.com>
2022-12-15 10:08:04 +0300

    ders 21-22-23

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import ReactDOM from "react-dom"
+import './index.css';
 
-
-
-function Greeting() {
+function BookList() {
     return (
-        <div>
-            <Person />
-            <Message />
-        </div>
+        <section className="booklist">
+            <Book />
+        </section>
     );
 }
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-    return <p>this is my message</p>;
+
+const author = 'Amelia Hepworth';
+const Book = () => {
+    const title = 'I Love You to the Moon and Back';
+    return (
+        <article className="book">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg" alt="" />
+            <h1>{ title }</h1>
+            <h4>{author.toUpperCase()}</h4>
+            {/* <p>{let x = 6 }</p> */}
+            <p>{6 + 6 }</p>
+        </article>
+    );
 };
 
-ReactDOM.render(<Greeting />, document.getElementById('root'));
+
+ReactDOM.render(<BookList />, document.getElementById('root'));

dfb1c38a05b2d44484aa9bc920b34ec5f59628f9
Kadir Zorlu <75729647+abdulkadirzorlu@users.noreply.github.com>
2023-01-09 08:59:30 +0300

    ders -45 local images(src folder)

--- src/books.js
+++ src/books.js
@@ -1,19 +1,22 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
 const Books = [
     {
         id: 1,
-        img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg',
+        img: img1,
         title: 'I Love You to the Moon and Back',
         author: 'Amelia Hepworth',
     },
     {
         id: 2,
-        img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+        img: img2,
         title: 'Our Class is a Family',
         author: 'Shannon Olsen',
     },
     {
         id: 3,
-        img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/81blQfKsLtL._AC_UL900_SR200,200_.jpg',
+        img: img3,
         title: 'Fairy Tale',
         author: 'Stephen King',
     },

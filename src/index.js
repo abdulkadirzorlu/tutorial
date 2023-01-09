2abba3967c2c218f9039f382ee4469a818e103af
Kadir Zorlu <75729647+abdulkadirzorlu@users.noreply.github.com>
2023-01-09 08:36:28 +0300

    ders-43 | complex example fix

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function BookList() {
         const book = books.find((book) => book.id === id);
         console.log(book);
     }
-    getBook(2);
+   // getBook(2);
     return (
         <section className="booklist">
            {books.map((book) =>{
@@ -45,11 +45,14 @@ function BookList() {
 const Book = (props) => {
     const { id, img, title, author, getBook } = props
     // console.log(props);
+    const getSingleBook = () => {
+        getBook(id)
+    }
     return (
         <article className="book" >
             <img src={img} alt="" />
             <h1>{title}</h1>
-            <button onClick={getBook(id)}>Click me</button>
+            <button onClick={()=> getBook(id)}>Click me</button>
             <h4>{author}</h4>
         </article>
     );
